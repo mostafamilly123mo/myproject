a54441732540ef87f68e2bb7ec4d0f060d6acd38
Mustafa Kashoul Melli <mostafamilly6@gmail.com>
2022-12-20 05:22:23 +0200

    Add mui library

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    main: "#87ff00",
+    light: "#bfff57",
+    dark: "#4bcb00",
+  },
+  secondary: {
+    main: "#b4bfff",
+    light: "#e7f2ff",
+    dark: "#838fcc",
+  },
+});
